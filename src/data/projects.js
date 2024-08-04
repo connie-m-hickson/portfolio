3216121692182609de06954dcef835ccffdcc291
Connie <connie.m.hickson@gmail.com>
2024-08-04 16:03:27 -0500

    Adding Additional Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,6 +11,54 @@ const projects = [
     imageUrl: "/assets/images/project2.jpg",
     link: "http://example.com/project-two",
   },
+  {
+    title: "Game Development",
+    description: "A brief description of Project One.",
+    imageUrl: "/assets/images/project1.jpg",
+    link: "http://example.com/project-one",
+  },
+  {
+    title: "Library Management System",
+    description: "A brief description of Project Two.",
+    imageUrl: "/assets/images/project2.jpg",
+    link: "http://example.com/project-two",
+  },
+  {
+    title: "Quiz Game",
+    description: "A brief description of Project One.",
+    imageUrl: "/assets/images/project1.jpg",
+    link: "http://example.com/project-one",
+  },
+  {
+    title: "Password Generator",
+    description: "A brief description of Project Two.",
+    imageUrl: "/assets/images/project2.jpg",
+    link: "http://example.com/project-two",
+  },
+  {
+    title: "News Aggregator",
+    description: "A brief description of Project One.",
+    imageUrl: "/assets/images/project1.jpg",
+    link: "http://example.com/project-one",
+  },
+  {
+    title: "Automated Testing Suite",
+    description: "A brief description of Project Two.",
+    imageUrl: "/assets/images/project2.jpg",
+    link: "http://example.com/project-two",
+  },
+  {
+    title: "Recipe Sharing Platform:",
+    description: "A brief description of Project One.",
+    imageUrl: "/assets/images/project1.jpg",
+    link: "http://example.com/project-one",
+  },
+  {
+    title: "Social Media Dashboard",
+    description: "A brief description of Project Two.",
+    imageUrl: "/assets/images/project2.jpg",
+    link: "http://example.com/project-two",
+  },
 ];
 
 export default projects;
